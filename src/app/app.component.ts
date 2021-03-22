897323a95df90f61ef3e757ff24bf26a01a4fe01
Nicholas Agwah <nagwah@teqbridgeltd.com>
2021-03-22 15:53:19 +0100

    ang_material_with_select_filter_columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
+import { HttpClient } from '@angular/common/http';
+import { MatPaginator } from '@angular/material/paginator';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -28,10 +11,223 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class AppComponent {
   title = 'matTable';
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource = new MatTableDataSource();
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'username',
+    'email',
+    'phone',
+    'website',
+  ];
 
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  filterValues = {};
+  filterSelectObj = [];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private http: HttpClient) {
+    this.filterSelectObj = [
+      {
+        name: 'ID',
+        columnProp: 'id',
+        options: [],
+      },
+      {
+        name: 'NAME',
+        columnProp: 'name',
+        options: [],
+      },
+      {
+        name: 'USERNAME',
+        columnProp: 'username',
+        options: [],
+      },
+      {
+        name: 'EMAIL',
+        columnProp: 'email',
+        options: [],
+      },
+      {
+        name: 'STATUS',
+        columnProp: 'status',
+        options: [],
+      },
+    ];
+  }
+
+  ngOnInit() {
+    this.getRemoteData();
+    this.dataSource.filterPredicate = this.createFilter();
+  }
+
+  getFilterObject(fullObj, key) {
+    const uniqChk = [];
+    fullObj.filter((obj) => {
+      if (!uniqChk.includes(obj[key])) {
+        uniqChk.push(obj[key]);
+      }
+      return obj;
+    });
+    return uniqChk;
+  }
+  getRemoteData() {
+    const remoteDummyData = [
+      {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        phone: '1-770-736-8031 x56442',
+        website: 'hildegard.org',
+        status: 'Active',
+      },
+      {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        phone: '010-692-6593 x09125',
+        website: 'anastasia.net',
+        status: 'Blocked',
+      },
+      {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+        phone: '1-463-123-4447',
+        website: 'ramiro.info',
+        status: 'Blocked',
+      },
+      {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+        phone: '493-170-9623 x156',
+        website: 'kale.biz',
+        status: 'Active',
+      },
+      {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca',
+        phone: '(254)954-1289',
+        website: 'demarco.info',
+        status: 'Active',
+      },
+      {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info',
+        phone: '1-477-935-8478 x6430',
+        website: 'ola.org',
+        status: 'In-Active',
+      },
+      {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+        phone: '210.067.6132',
+        website: 'elvis.io',
+        status: 'Active',
+      },
+      {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me',
+        phone: '586.493.6943 x140',
+        website: 'jacynthe.com',
+        status: 'In-Active',
+      },
+      {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io',
+        phone: '(775)976-6794 x41206',
+        website: 'conrad.com',
+        status: 'In-Active',
+      },
+      {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        phone: '024-648-3804',
+        website: 'ambrose.net',
+        status: 'Active',
+      },
+    ];
+    this.dataSource.data = remoteDummyData;
+    this.filterSelectObj.filter((o) => {
+      o.options = this.getFilterObject(remoteDummyData, o.columnProp);
+    });
+  }
+
+  // Called on Filter change
+
+  filterChange(filter, event) {
+    this.filterValues[
+      filter.columnProp
+    ] = event.target.value.trim().toLowerCase();
+    this.dataSource.filter = JSON.stringify(this.filterValues);
+  }
+  // Custom filter method fot Angular Material Datatable
+  createFilter() {
+    let filterFunction = function (data: any, filter: string): boolean {
+      let searchTerms = JSON.parse(filter);
+      let isFilterSet = false;
+      for (const col in searchTerms) {
+        if (searchTerms[col].toString() !== '') {
+          isFilterSet = true;
+        } else {
+          delete searchTerms[col];
+        }
+      }
+
+      console.log(searchTerms);
+
+      let nameSearch = () => {
+        let found = false;
+        if (isFilterSet) {
+          for (const col in searchTerms) {
+            searchTerms[col]
+              .trim()
+              .toLowerCase()
+              .split(' ')
+              .forEach((word) => {
+                if (
+                  data[col].toString().toLowerCase().indexOf(word) != -1 &&
+                  isFilterSet
+                ) {
+                  found = true;
+                }
+              });
+          }
+          return found;
+        } else {
+          return true;
+        }
+      };
+      return nameSearch();
+    };
+    return filterFunction;
+  }
+
+  // Reset table filters
+  resetFilters() {
+    this.filterSelectObj.forEach((value, key) => {
+      value.modelValue = undefined;
+    });
+    this.dataSource.filter = '';
+  }
+  //Paginator
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,10 +20,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     BrowserModule,
     MatTableModule,
     MatInputModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
     MatFormFieldModule,
     ReactiveFormsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
